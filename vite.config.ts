2ef8a24773e89ef2832aa833f046272b6d060570
Jianan <jiananh99@gmail.com>
2025-12-03 17:11:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
+
     return {
+      base: '/',   
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env': env   
       },
       resolve: {
         alias: {
@@ -21,3 +22,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
